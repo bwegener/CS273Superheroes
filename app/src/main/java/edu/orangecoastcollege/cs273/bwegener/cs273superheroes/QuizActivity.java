97fbbcd588c364cec2dbe029d701758c9341d188
bwegener <wegener_brian@yahoo.com>
2017-10-12 14:19:25 -0700

    Fixed Handler

--- app/src/main/java/edu/orangecoastcollege/cs273/bwegener/cs273superheroes/QuizActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/bwegener/cs273superheroes/QuizActivity.java
@@ -3,17 +3,16 @@ package edu.orangecoastcollege.cs273.bwegener.cs273superheroes;
 import android.content.DialogInterface;
 import android.content.res.AssetManager;
 import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -21,8 +20,6 @@ import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.logging.Handler;
-import java.util.logging.LogRecord;
 
 public class QuizActivity extends AppCompatActivity {
 
